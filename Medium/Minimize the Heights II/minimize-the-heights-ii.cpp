2e3a37c30d19e0ec205e9a84c6ec1c0c77cd2032
Vansh Garg <153756516+vanshgarg-1@users.noreply.github.com>
2024-03-04 22:43:12 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        sort(arr,arr+n);
+        
+        int maxi=arr[n-1];
+        int mini=arr[0];
+        
+        int res=arr[n-1]-arr[0];
+        
+        
+        for(int i=1;i<n;i++){
+            mini=min(arr[0]+k,arr[i]-k);
+            maxi=max(arr[n-1]-k,arr[i-1]+k);
+            
+            if(mini<0)   //Take care of the -ve case:
+                continue;
+            
+            res=min(res,maxi-mini);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getMinDiff(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
