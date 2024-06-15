da9e31444f113780df917b5f4bdd71ca978d5c80
Vansh Garg <gargvansh.in@gmail.com>
2024-06-15 14:40:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Outermost Parentheses/outermost-parentheses.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string removeOuter(string& s) {
+        string res;
+        int opened = 0;
+        for (char c : s) {
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+
+        cout << obj.removeOuter(s) << "\n";
+    }
+}
+// } Driver Code Ends
